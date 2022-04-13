cfaf414204e504d944395078475d7673c5293a31
Chris Britton <chrisbritton790@gmail.com>
2022-04-13 15:35:32 -0700

    added html to SplashPage

---
+++ react-app/src/components/SplashPage/index.js
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+const SplashPage = () => {
+    return (
+        <div className="splash-wrapper">
+            <div className="header-bar">
+                <div className="nav-container">
+                    <div className="svg-logo">
+
+                    </div>
+                    <div className="nav-btns">
+                        <button className="top-sign-up">SIGN UP</button>
+                        <button className="top-login">LOGIN</button>
+
+                    </div>
+                </div>
+            </div>
+            <div className="banner-container">
+                <div className="left-banner">
+                    <h1>Easy-to-Use Online Form Builder For Every Organization</h1>
+                    <p>Cloud-based form builder that makes it easy to create registration forms, application forms, surveys, contact forms, payment forms and more.</p>
+                    <div className="banner-btn-row">
+                        <button className="sign-up-free">SIGN UP FREE</button>
+                        <button className="live-demo">LIVE DEMO</button>
+                    </div>
+                </div>
+                <div className="right-banner">
+
+                </div>
+            </div>
+        </div>
+    )
+}
