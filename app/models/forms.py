d502b94d15184c5fbf39f53372309b7e7d90f5de
Chris Britton <chrisbritton790@gmail.com>
2022-04-12 22:09:11 -0700

    started form model

---
+++ app/models/forms.py
@@ -0,0 +1,18 @@
+from .db import db
+
+class Form(db.Model):
+    __tablename__ = 'forms'
+
+    id = db.Column(db.Integer, primary_key=True)
+    form_url = db.Column(db.String(255))
+    title = db.Column(db.String(255))
+    field_type = db.Column(db.JSONB, nullable=False)
+    field_labels = db.Column(db.JSONB)
+    input_labels = db.Column(db.JSONB)
+    user_id = db.Column(db.Integer)
+    all_entries = db.Column(db.Integer)
+    
+
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    hashed_password = db.Column(db.String(255), nullable=False)
