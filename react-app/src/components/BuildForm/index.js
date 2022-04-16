e287255a32672cbc514a75904e2ab91ec30faf0a
Chris Britton <chrisbritton790@gmail.com>
2022-04-15 23:11:38 -0700

    added edit panel

--- react-app/src/components/BuildForm/index.js
+++ react-app/src/components/BuildForm/index.js
@@ -7,7 +7,8 @@ import './BuildForm.css'
 const BuildForm = () => {
     const dispatch = useDispatch();
     const [inputs, setInputs] = useState([]);
-    console.log('Inputs', inputs)
+    const [showEdit, setShowEdit] = useState(false);
+    const [selectedFieldIndex, setSelectedFieldIndex] = useState(null);
 
     const handleLogout = () => {
         dispatch(sessionActions.logout());
@@ -41,6 +42,12 @@ const BuildForm = () => {
         setInputs((inputs) => [...inputs, 7, ]);
     };
 
+    const handleFieldSelect = (e) => {
+        e.preventDefault();
+        setSelectedFieldIndex(parseInt(e.target.id, 10));
+        setShowEdit(true);
+    };
+
     return (
         <div className="build-form-wrapper">
             <div className="build-container">
@@ -64,32 +71,48 @@ const BuildForm = () => {
                 </div>
                 <div className="form-stage">
                     <div className="left-side">
-                        <h3>Standard</h3>
-                        <ul className="left-input-col">
-                            <li className="text-li">
-                                <button type="button" onClick={handleText}>Single Line Text</button>
-                            </li>
-                            <li className="paragraph-li">
-                                <button type="button" onClick={handleParagraph}>Paragraph Text</button>
-                            </li>
-                            <li className="number-li">
-                                <button type="button" onClick={handleNumber}>Number</button>
-                            </li>
-                            <li className="url-li">
-                                <button type="button" onClick={handleUrl}>Url</button>
-                            </li>
-                        </ul>
-                        <ul className="right-input-col">
-                            <li className="email-li">
-                                <button type="button" onClick={handleEmail}>Email</button>
-                            </li>
-                            <li className="telephone-li">
-                                <button type="button" onClick={handleTelephone}>Phone</button>
-                            </li>
-                            <li className="date-li">
-                                <button type="button" onClick={handleDate}>Date</button>
-                            </li>
-                        </ul>
+                        {!showEdit && (
+                            <>
+                                <h3>Standard</h3>
+                                <ul className="left-input-col">
+                                    <li className="text-li">
+                                        <button type="button" onClick={handleText}>Single Line Text</button>
+                                    </li>
+                                    <li className="paragraph-li">
+                                        <button type="button" onClick={handleParagraph}>Paragraph Text</button>
+                                    </li>
+                                    <li className="number-li">
+                                        <button type="button" onClick={handleNumber}>Number</button>
+                                    </li>
+                                    <li className="url-li">
+                                        <button type="button" onClick={handleUrl}>Url</button>
+                                    </li>
+                                </ul>
+                                <ul className="right-input-col">
+                                    <li className="email-li">
+                                        <button type="button" onClick={handleEmail}>Email</button>
+                                    </li>
+                                    <li className="telephone-li">
+                                        <button type="button" onClick={handleTelephone}>Phone</button>
+                                    </li>
+                                    <li className="date-li">
+                                        <button type="button" onClick={handleDate}>Date</button>
+                                    </li>
+                                </ul>
+                            </>
+                        )}
+                        {showEdit && (
+                            <>
+                                <div>
+                                    <label for="label-edit-field">Field Label</label>
+                                    <textarea id="label-edit-field"></textarea>
+                                </div>
+                                <div>
+                                    <button>Apply Label</button>
+                                    <button>Delete Field</button>
+                                </div>
+                            </>
+                        )}
                     </div>
                     <div className="form-main">
                         <div className="form-info-div">
@@ -103,43 +126,43 @@ const BuildForm = () => {
                                         {input === 1 && (
                                             <div>
                                                 <label for={`${idx}`}></label>
-                                                <input id={`${idx}`} type="text"></input>
+                                                <input id={`${idx}`} type="text" onClick={handleFieldSelect}></input>
                                             </div>
                                         )}
                                         {input === 2 && (
                                             <div>
                                                 <label for={`${idx}`}></label>
-                                                <textarea id={`${idx}`}></textarea>
+                                                <textarea id={`${idx}`} onClick={handleFieldSelect}></textarea>
                                             </div>
                                         )}
                                         {input === 3 && (
                                             <div>
                                                 <label for={`${idx}`}></label>
-                                                <input id={`${idx}`} type="number"></input>
+                                                <input id={`${idx}`} type="number" onClick={handleFieldSelect}></input>
                                             </div>
                                         )}
                                         {input === 4 && (
                                             <div>
                                                 <label for={`${idx}`}></label>
-                                                <input id={`${idx}`} type="url"></input>
+                                                <input id={`${idx}`} type="url" onClick={handleFieldSelect}></input>
                                             </div>
                                         )}
                                         {input === 5 && (
                                             <div>
                                                 <label for={`${idx}`}></label>
-                                                <input id={`${idx}`} type="email"></input>
+                                                <input id={`${idx}`} type="email" onClick={handleFieldSelect}></input>
                                             </div>
                                         )}
                                         {input === 6 && (
                                             <div>
                                                 <label for={`${idx}`}></label>
-                                                <input id={`${idx}`} type="telephone"></input>
+                                                <input id={`${idx}`} type="telephone" onClick={handleFieldSelect}></input>
                                             </div>
                                         )}
                                         {input === 7 && (
                                             <div>
                                                 <label for={`${idx}`}></label>
-                                                <input id={`${idx}`} type="date"></input>
+                                                <input id={`${idx}`} type="date" onClick={handleFieldSelect}></input>
                                             </div>
                                         )}
                                     </>
