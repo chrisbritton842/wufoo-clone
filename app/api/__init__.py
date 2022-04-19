10db07280d9094eea92aca8770441b50faf6a02d
Chris Britton <chrisbritton790@gmail.com>
2022-04-19 15:13:15 -0700

    set up api for entries

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,3 +1,4 @@
 from .auth_routes import auth_routes
 from .user_routes import user_routes
 from .form_routes import form_routes
+from .entry_routes import entry_routes
---
+++ app/api/entry_routes.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, request
+from app.models import db, Entry
+
+entry_routes = Blueprint('entrie', __name__)
+
+@entry_routes.route('/<formId>', methods=['GET', 'POST'])
+def add_entry(formId):
+
+    if request.method == 'POST':
+
+        data = request.get_json(force=True)
+
+        entry = Entry(date=data["date"], email=data["email"], number=data["number"], telephone=data["telephone"], text=data["text"], textarea=data["textArea"], url=data["url"], form_id=formId)
+        db.session.add(entry)
+        db.session.flush()
+        db.session.commit()
+
+        entries = Entry.query.all()
+
+        return { "entries": sorted([e.to_dict() for e in entries], key=lambda e: e["created_at"]) }
+
+    entries = Entry.query.all()
+
+    return { "entries": sorted([e.to_dict() for e in entries], key=lambda e: e["created_at"]) }
