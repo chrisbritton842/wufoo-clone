b1605f508d96b80a5d38f29f41dbbf4296a2a8bc
Chris Britton <chrisbritton790@gmail.com>
2022-04-19 13:50:51 -0700

    field states set up

--- react-app/src/components/ViewForm/index.js
+++ react-app/src/components/ViewForm/index.js
@@ -16,9 +16,47 @@ const ViewForm = () => {
     const labels = userForm?.field_labels;
     const title = userForm?.title;
     const description = userForm?.input_labels;
+    const [date, setDate] = useState({});
+    const [email, setEmail] = useState({});
+    const [number, setNumber] = useState({});
+    const [telephone, setTelephone] = useState({});
+    const [text, setText] = useState({});
+    const [textArea, setTextArea] = useState({});
+    const [url, setUrl] = useState({});
+
+    console.log("Email: ", email)
+    console.log("Url: ", url)
 
     const handleSubmitForm = () => {
 
+    };
+
+    const handleDateInput = (e) => {
+        setDate({...date, [e.target.dataset.label]: e.target.value});
+    };
+
+    const handleEmailInput = (e) => {
+        setEmail({...email, [e.target.dataset.label]: e.target.value});
+    };
+
+    const handleNumberInput = (e) => {
+        setNumber({...number, [e.target.dataset.label]: e.target.value});
+    };
+
+    const handleTelephoneInput = (e) => {
+        setTelephone({...telephone, [e.target.dataset.label]: e.target.value});
+    };
+
+    const handleTextInput = (e) => {
+        setText({...text, [e.target.dataset.label]: e.target.value});
+    };
+
+    const handleTextAreaInput = (e) => {
+        setTextArea({...textArea, [e.target.dataset.label]: e.target.value});
+    };
+
+    const handleUrlInput = (e) => {
+        setUrl({...url, [e.target.dataset.label]: e.target.value});
     };
 
     useEffect(() => {
@@ -41,43 +79,43 @@ const ViewForm = () => {
                                         {input === 1 && (
                                             <div>
                                                 <label for={`${idx}`}>{labels[idx]}</label>
-                                                <input id={`${idx}`} type="text"></input>
+                                                <input id={`${idx}`} type="text" data-label={labels[idx]} value={text[labels[idx]]} onChange={handleTextInput}></input>
                                             </div>
                                         )}
                                         {input === 2 && (
                                             <div>
                                                 <label for={`${idx}`}>{labels[idx]}</label>
-                                                <textarea id={`${idx}`}></textarea>
+                                                <textarea id={`${idx}`} data-label={labels[idx]} value={textArea[labels[idx]]} onChange={handleTextAreaInput}></textarea>
                                             </div>
                                         )}
                                         {input === 3 && (
                                             <div>
                                                 <label for={`${idx}`}>{labels[idx]}</label>
-                                                <input id={`${idx}`} type="number"></input>
+                                                <input id={`${idx}`} type="number" data-label={labels[idx]} value={number[labels[idx]]} onChange={handleNumberInput}></input>
                                             </div>
                                         )}
                                         {input === 4 && (
                                             <div>
                                                 <label for={`${idx}`}>{labels[idx]}</label>
-                                                <input id={`${idx}`} type="url"></input>
+                                                <input id={`${idx}`} type="url" data-label={labels[idx]} value={url[labels[idx]]} onChange={handleUrlInput}></input>
                                             </div>
                                         )}
                                         {input === 5 && (
                                             <div>
                                                 <label for={`${idx}`}>{labels[idx]}</label>
-                                                <input id={`${idx}`} type="email"></input>
+                                                <input id={`${idx}`} type="email" data-label={labels[idx]} value={email[labels[idx]]} onChange={handleEmailInput}></input>
                                             </div>
                                         )}
                                         {input === 6 && (
                                             <div>
                                                 <label for={`${idx}`}>{labels[idx]}</label>
-                                                <input id={`${idx}`} type="telephone"></input>
+                                                <input id={`${idx}`} type="telephone" data-label={labels[idx]} value={telephone[labels[idx]]} onChange={handleTelephoneInput}></input>
                                             </div>
                                         )}
                                         {input === 7 && (
                                             <div>
                                                 <label for={`${idx}`}>{labels[idx]}</label>
-                                                <input id={`${idx}`} type="date"></input>
+                                                <input id={`${idx}`} type="date" data-label={labels[idx]} value={date[labels[idx]]} onChange={handleDateInput}></input>
                                             </div>
                                         )}
                                     </>
