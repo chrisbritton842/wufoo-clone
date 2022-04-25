1e7bf075a87894534e1127e000279541b0dfd0b1
Chris Britton <chrisbritton790@gmail.com>
2022-04-25 00:11:00 -0700

    added end page

--- react-app/src/components/EndPage/index.js
+++ react-app/src/components/EndPage/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import './EndPage.css';
 
 const EndPage = () => {
     let history = useHistory();
@@ -11,13 +12,17 @@ const EndPage = () => {
     function handleLogin() {
         history.push('/login');
     };
-    
+
+    const handleLogo = () => {
+        history.push('/');
+    };
+
     return (
         <div className="end-page-wrapper">
-            <div className="header-bar">
-                <div className="nav-container">
+            <div className="end-header-bar">
+                <div className="end-nav-container">
                     <div className="svg-logo">
-
+                        <img className="rare-form-logo-user-forms" src="../../../../static/signup.png" alt="Logo for RareForm website" style={{height: "50px"}} onClick={handleLogo}></img>
                     </div>
                     <div className="nav-btns">
                         <button type="button" className="top-sign-up" onClick={handleSignUp}>SIGN UP</button>
@@ -26,7 +31,10 @@ const EndPage = () => {
                 </div>
             </div>
             <div className="end-banner-text">
-
+                <div className="end-banner-content">
+                    <h1>Thanks for filling out my form!</h1>
+                    <p>Want to create your own form? It's free, quick, and easy!</p>
+                </div>
             </div>
         </div>
     )
