3072b2e0be932ec81ac0456d201b7a76c3f71b02
Chris Britton <chrisbritton790@gmail.com>
2022-12-01 22:29:55 -0800

    modified model files to include schema

--- app/models/form.py
+++ app/models/form.py
@@ -1,10 +1,13 @@
-from .db import db
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 from sqlalchemy import DateTime
 from sqlalchemy.sql import func
 from sqlalchemy.dialects.postgresql import JSONB
 
 class Form(db.Model):
     __tablename__ = 'forms'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
     form_url = db.Column(db.String(255))
